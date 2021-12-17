6d80b6cfd26cd70ca58f21a831b58c9b7013c35b
Peter Akarakiri <p.akarakiri@gmail.com>
2021-12-16 22:46:06 -0800

    added initial basic tests

---
+++ __tests__/home.test.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Home from '../pages/index'
+
+describe('Home', () => {
+    it('should render the heading', () => {
+        render(<Home />)
+
+        const heading = screen.getByText(
+            /Note App/i,
+        )
+
+        expect(heading).toBeInTheDocument()
+    })
+})
+
+describe('Home', () => {
+    it(`should render "Create Note" button`, () => {
+        render(<Home />)
+
+        const createNote = screen.getByRole('button', {
+                name: /Create Note/i,
+            }
+        );
+
+        expect(createNote).toBeInTheDocument()
+    })
+})
