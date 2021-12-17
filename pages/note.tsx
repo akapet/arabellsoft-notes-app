ee414e0149109f2d1eb97611541038d4ea06970f
Peter Akarakiri <p.akarakiri@gmail.com>
2021-12-16 16:23:08 -0800

    added date when user created the note

--- pages/note.tsx
+++ pages/note.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Icon } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
+import moment from 'moment';
 import { Note as NoteData } from '../data/Note';
+
 
 function Note(props) {
   const { note, handleEditNote } = props
   
   return (
-    <Card fluid style={{ width: 500, height: 80, cursor: 'pointer', margin: '0.01em' }} onClick={() => handleEditNote(note)}>
+    <Card fluid style={{ width: 500, height: 110, cursor: 'pointer', margin: '0.01em' }} onClick={() => handleEditNote(note)}>
       <Card.Content>
         <Card.Header content={getTitle(note)} />
         <Card.Meta content={note.content} />
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          {`Created ${moment(note.date).fromNow()}`}
+        </a>
       </Card.Content>
     </Card>   
   );
